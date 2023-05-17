2997eb6d8594f5b4ba7d4817e93fad874d9131dd
unknown <maria.rodriguez@northwestern.edu>
2023-05-16 20:58:13 -0500

    scripts and client

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -14,9 +14,6 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
-    devtool: {
-      devtool: 'source-map',
-    },
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
